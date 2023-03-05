4265ba121f80c18d8f71816437b1a60b36d6d157
galang hanafi <galanghanafi9131@gmail.com>
2023-03-05 19:16:11 +0700

    Attendance belongsTo Student

--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -3,9 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\Student;
 
 class Attendance extends Model
 {
-    
+    use SoftDeletes;
+
+    public function student()
+    {
+        return $this->belongsTo(Student::class);
+    }
 }
--- app/Models/Student.php
+++ app/Models/Student.php
@@ -3,9 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Student extends Model
 {
-    
+    use SoftDeletes;
+
 }
